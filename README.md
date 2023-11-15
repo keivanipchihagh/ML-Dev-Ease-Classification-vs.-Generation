fe2a0a3224502f481ea1e25f9780590d345267ee
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2023-11-15 21:19:55 +0330

    feat(task2): add data and scraper code

--- README.md
+++ README.md
@@ -3,14 +3,21 @@ An Exploratory (Data) Analysis on Development Ease in ML Applications - Text Cla
 
 
 ## Table of Contents
-- Task 1
+- [Step 1](https://github.com/keivanipchihagh/ML-Dev-Ease-Classification-vs.-Generation#step-1)
+- [Step 2](https://github.com/keivanipchihagh/ML-Dev-Ease-Classification-vs.-Generation#step-2)
 
 
 
 
-## Task 1
-A simple scraper under `src/scrapers/models.py` is developed to obtain the models for both *Text-Classification* and *Text-Generation*, then store them under `data/models.csv`. Furthermore, the script supports arguments to change the number of models to retrieve (default: 20) and the sorting strategy (default: likes). A usage example is provided below:
+## Step 1 - Obtaining Popular Models
+A simple scraper is developed under `src/scrapers/models.py` using [huggingface library](https://pypi.org/project/huggingface-hub/). This script is used to retrieve models for both *Text-Classification* and *Text-Generation* and store them under `data/models.csv`. Furthermore, the script supports arguments to customize the results. A usage example is provided below:
 
 ```python
 python .\src\scrapers\models.py --sort likes --count 20
 ```
+
+## Step 2 - Obtaining ML Spaces
+For this purpose, another scraper is developed under `src/scrapers/spaces.csv` and its process is broken down into two steps. First, a list of all spaces is acquired under `data/spaces.csv` which contains the names of *175k* ML apps. Then, using parallelism, more details of each space is retrieved (i.e. models and datasets).
+
+> **Warning**
+> Although it's very unlikely, parallelism could possibly result in DDOS attack, and thus should be used with caution.
---
+++ src/scrapers/spaces.py
@@ -0,0 +1,91 @@
+##########################################################
+#
+# Copyright (C) 2023-PRESENT: Keivan Ipchi Hagh
+#
+# Email:            keivanipchihagh@gmail.com
+# GitHub:           https://github.com/keivanipchihagh
+#
+##########################################################
+
+import os
+import pandas as pd
+import huggingface_hub
+from dotenv import load_dotenv
+from pandarallel import pandarallel
+from typing import Iterable, List
+from huggingface_hub.hf_api import SpaceInfo
+
+load_dotenv()
+pandarallel.initialize(
+    progress_bar = True,
+    nb_workers = 32
+)
+
+
+def collect_spaces(spaces: Iterable[SpaceInfo]) -> List[dict]:
+    """
+        Collects spaces data
+
+        Parameters:
+            spaces (Iterable[ModelInfo]): Spaces iterable
+        Returns:
+            (List[dict]): List of dictionaries containing data for each space
+    """
+    data = []
+    for _ in spaces:
+        data.append({
+            'name': _.id,
+            'likes': _.likes,
+        })
+    return data
+
+
+
+def collect_space_info(row: pd.Series) -> pd.Series:
+    """
+        Collect extra information about each space
+
+        Parameters:
+            row (pd.Series): Single DataFrame row
+        Returns:
+            (pd.Series): Modified row
+    """
+    import huggingface_hub
+    TOKEN = os.getenv("HUGGING_FACE_TOKEN")
+
+    _ = huggingface_hub.space_info(
+        repo_id = row['name'],
+        token = TOKEN,
+    )
+    row['author'] = _.author
+    row['last_modified'] = _.author
+    # models (may not be available)
+    try:
+        row['models'] = _.models
+    except:
+        row['models'] = None
+    # datasets (may not be available)
+    try:
+        row['datasets'] = _.models
+    except:
+        row['datasets'] = None
+
+    return row
+
+
+if __name__ == '__main__':
+
+    # Retrieve Models from HuggingFace
+    spaces = huggingface_hub.list_spaces()
+
+    data = collect_spaces(spaces)   # Collect spaces list
+    df = pd.DataFrame(data = data)  # Convert to DataFrame
+    df.to_csv('data/spaces.csv', index=False)
+
+    # Collect extra information for spaces
+    # NOTE: Using pandarallel would run the function on all cores, and is likely to simulate a small DDOS attack
+    #       on HuggingFace API servers, and thus may not be a good solution. However, it's x64 faster than normal (:
+    df = df.parallel_apply(collect_space_info, axis=1)
+    # df = df.apply(collect_space_info, axis=1)
+
+    df.to_csv('data/spaces_extra.csv', index=False)
